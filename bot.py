cdf2e30957b560f463a743fa16f2128508731451
Puzanovim <puzanovim@yandex.ru>
2020-12-15 14:31:38 +0400

    update db funcs

--- bot.py
+++ bot.py
@@ -52,19 +52,19 @@ db = Db()
 #             print(e)
 
 
-@dp.message_handler(commands=['get_name_alert_all'])
-async def send_cert(message: types.Message):
-    users = db.get_users()
-    for user in users:
-        try:
-            name = user['name']
-            user_id = user['user_id']
-            text = MESSAGES['alert_change_name'] + name
-            await bot.send_message(user_id, text)
-            print(f"Alert to {name} with id={user_id} sent!")
-            await sleep(1)
-        except Exception as e:
-            print(e)
+# @dp.message_handler(commands=['get_name_alert_all'])
+# async def send_cert(message: types.Message):
+#     users = db.get_users()
+#     for user in users:
+#         try:
+#             name = user['name']
+#             user_id = user['user_id']
+#             text = MESSAGES['alert_change_name'] + name
+#             await bot.send_message(user_id, text)
+#             print(f"Alert to {name} with id={user_id} sent!")
+#             await sleep(1)
+#         except Exception as e:
+#             print(e)
 
 
 @dp.message_handler(commands=['start'])
@@ -96,7 +96,7 @@ async def process_name_team(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
 
-    # db.create_team(message.from_user.id, message.text)
+    db.create_team(message.from_user.id, message.text)
     await Registration.next()
     await message.reply(MESSAGES['contact_face'], reply=False)
 
@@ -109,7 +109,7 @@ async def process_contact_face(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
 
-    # db.add_contact_face(message.from_user.id, message.text)
+    db.add_contact_face(message.from_user.id, message.text)
     await Registration.next()
     await message.reply(MESSAGES['link_vk'], reply=False)
 
@@ -122,7 +122,7 @@ async def process_link_vk(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['link_vk'] = message.text
 
-    # db.add_link_vk(message.from_user.id, message.text)
+    db.add_link_vk(message.from_user.id, message.text)
     await Registration.next()
     await message.reply(MESSAGES['tel_number'], reply=False)
 
@@ -135,7 +135,7 @@ async def process_tel_number(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['tel_number'] = message.text
 
-    # db.add_tel_number(message.from_user.id, message.text)
+    db.add_tel_number(message.from_user.id, message.text)
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     for institut in institutes.keys():
         institut_btn = KeyboardButton(institut)
@@ -153,84 +153,109 @@ async def process_institute(message: types.Message, state: FSMContext):
         data['institute'] = message.text
 
     # добавляем институт в БД
-    # db.add_institute(message.from_user.id, message.text)
+    db.add_institute(message.from_user.id, message.text)
     await state.finish()
     await message.reply(MESSAGES['end_reg'], reply=False)
 
 
 @dp.message_handler(commands=['start_Quiz'])
 async def start_quiz(message: types.Message):
-    if db.get_current_question(user_id=message.from_user.id) > 30:
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    if db.get_current_question(user_id=message.from_user.id) > 14:
         text = MESSAGES['you are finished']
         markup = ReplyKeyboardRemove()
+        await message.reply(text["Place"], reply_markup=markup, reply=False)
     else:
         await Quest.Task1.set()
         db.increment_current_question(message.from_user.id, 0)
-        text = questions[1]['Question']
-        markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        for choice in questions[1]['Choices']:
-            choice_btn = KeyboardButton(choice)
-            markup.add(choice_btn)
-    await message.reply(text, reply_markup=markup, reply=False)
-
-
-@dp.message_handler(state=Quest)
-async def questions_step_by_step(message: types.Message, state: FSMContext):
-    """
-    Функция квиза
-    :param state:
-    :param message: сообщение пользователя
-    :return: отправляем следующий вопрос
-    """
-    user_id: int = message.from_user.id
-    current_question: int = db.get_current_question(user_id)
-
-    async with state.proxy() as data:
-        data[current_question] = message.text
-
-    right_answer = False
-    if current_question > 30:
-        text = MESSAGES['you are finished']
-        markup = ReplyKeyboardRemove()
-    else:
+        current_question = 1
         question = questions[current_question]
-        markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-        user_answer = message.text.lower()
-        true_answer = questions[current_question]['Answer'].lower()
+        await message.reply(question["Place"], reply_markup=markup, reply=False)
+        text = question['Question']
 
-        if user_answer == true_answer:
-            right_answer = True
-            db.set_point(user_id, current_question)
-
-        have_hint = db.have_hints(user_id)
-        if current_question + 1 > 20:
-            markup = ReplyKeyboardRemove()
-            #  here open questions
-            if not right_answer and not have_hint:
-                # даем подсказку
-                text = MESSAGES['hint'] + question['Choices'][0]
-                db.set_hint(user_id)
-            else:
-                # он дал верный ответ или уже была одна подсказка
-                db.delete_hint(user_id)
-                db.increment_current_question(user_id, current_question)
-                if current_question == 30:
-                    text = MESSAGES['the_end']
-                    await state.finish()
-                else:
-                    question = questions[current_question + 1]
-                    text = question['Question']
-                    await Quest.next()
-        else:
-            #  here quiz questions
-            db.increment_current_question(user_id, current_question)
-            question = questions[current_question + 1]
-            text = question['Question']
+        # markup
+        if len(question['Choices']) > 1:
             for choice in question['Choices']:
                 choice_btn = KeyboardButton(choice)
                 markup.add(choice_btn)
-            await Quest.next()
-    await message.reply(text, reply_markup=markup, reply=False)
+        else:
+            markup = ReplyKeyboardRemove()
+        # send question
+        await message.reply(text, reply_markup=markup, reply=False)
+        # add photo
+        if question["Photo"]:
+            try:
+                img = "yura.png"
+                await bot.send_photo(message.from_user.id, photo=open(img, 'rb'), caption=MESSAGES["duck"])
+            except Exception as e:
+                print(e)
+        # add duck task
+        if question["Duck"]:
+            try:
+                img = str(current_question) + ".png"
+                await bot.send_photo(message.from_user.id, photo=open(img, 'rb'), caption=MESSAGES["duck"])
+            except Exception as e:
+                print(e)
+                await message.reply(MESSAGES["duck"] + MESSAGES["error_photo"], reply_markup=markup, reply=False)
+
+
+# @dp.message_handler(state=Quest)
+# async def questions_step_by_step(message: types.Message, state: FSMContext):
+#     """
+#     Функция квиза
+#     :param state:
+#     :param message: сообщение пользователя
+#     :return: отправляем следующий вопрос
+#     """
+#     user_id: int = message.from_user.id
+#     current_question: int = db.get_current_question(user_id)
+#
+#     async with state.proxy() as data:
+#         data[current_question] = message.text
+#
+#     right_answer = False
+#     if current_question > 30:
+#         text = MESSAGES['you are finished']
+#         markup = ReplyKeyboardRemove()
+#     else:
+#         question = questions[current_question]
+#         markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+#         user_answer = message.text.lower()
+#         true_answer = questions[current_question]['Answer'].lower()
+#
+#         if user_answer == true_answer:
+#             right_answer = True
+#             db.set_point(user_id, current_question)
+#
+#         have_hint = db.have_hints(user_id)
+#         if current_question + 1 > 20:
+#             markup = ReplyKeyboardRemove()
+#             #  here open questions
+#             if not right_answer and not have_hint:
+#                 # даем подсказку
+#                 text = MESSAGES['hint'] + question['Choices'][0]
+#                 db.set_hint(user_id)
+#             else:
+#                 # он дал верный ответ или уже была одна подсказка
+#                 db.delete_hint(user_id)
+#                 db.increment_current_question(user_id, current_question)
+#                 if current_question == 30:
+#                     text = MESSAGES['the_end']
+#                     await state.finish()
+#                 else:
+#                     question = questions[current_question + 1]
+#                     text = question['Question']
+#                     await Quest.next()
+#         else:
+#             #  here quiz questions
+#             db.increment_current_question(user_id, current_question)
+#             question = questions[current_question + 1]
+#             text = question['Question']
+#             for choice in question['Choices']:
+#                 choice_btn = KeyboardButton(choice)
+#                 markup.add(choice_btn)
+#             await Quest.next()
+#     await message.reply(text, reply_markup=markup, reply=False)
 
 
 @dp.message_handler(state=Quest)
@@ -238,20 +263,45 @@ async def work_func(message: types.Message, state: FSMContext):
     user_answer = message.text.lower()
     user_id: int = message.from_user.id
     current_question: int = db.get_current_question(user_id)
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
     async with state.proxy() as data:
         data[current_question] = message.text
 
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    if current_question > 14:
+        text = MESSAGES['you are finished']
+        markup = ReplyKeyboardRemove()
+        await message.reply(text, reply_markup=markup, reply=False)
+
     if user_answer == "поехали!":  # задаем следующий вопрос
         question = questions[current_question]
+        await message.reply(question["Place"], reply_markup=markup, reply=False)
         text = question['Question']
+
+        # markup
         if len(question['Choices']) > 1:
             for choice in question['Choices']:
                 choice_btn = KeyboardButton(choice)
                 markup.add(choice_btn)
         else:
             markup = ReplyKeyboardRemove()
+        # send question
+        await message.reply(text, reply_markup=markup, reply=False)
+        # add photo
+        if question["Photo"]:
+            try:
+                img = "yura.png"
+                await bot.send_photo(message.from_user.id, photo=open(img, 'rb'), caption=MESSAGES["duck"])
+            except Exception as e:
+                print(e)
+        # add duck task
+        if question["Duck"]:
+            try:
+                img = str(current_question) + ".png"
+                await bot.send_photo(message.from_user.id, photo=open(img, 'rb'), caption=MESSAGES["duck"])
+            except Exception as e:
+                print(e)
+                await message.reply(MESSAGES["duck"] + MESSAGES["error_photo"], reply_markup=markup, reply=False)
     else:
         # if user_answer[0] != ".":  # проверяем текущий вопрос
         question = questions[current_question]
@@ -261,6 +311,7 @@ async def work_func(message: types.Message, state: FSMContext):
             db.set_point(user_id, current_question)
 
         if current_question == 14:
+            db.increment_current_question(user_id, current_question)
             text = MESSAGES['the_end']
             await state.finish()
         else:
@@ -269,9 +320,7 @@ async def work_func(message: types.Message, state: FSMContext):
             go_btn = KeyboardButton("Поехали!")
             markup.add(go_btn)
             await Quest.next()
-    await message.reply(text, reply_markup=markup, reply=False)
-
-
+        await message.reply(text, reply_markup=markup, reply=False)
 
 
 @dp.message_handler()
--- db.py
+++ db.py
@@ -29,7 +29,7 @@ class Db:
         return cursor
 
     def count(self, user_id):
-        sql = "SELECT COUNT(*) FROM `leader_users` WHERE `user_id`=%s"
+        sql = "SELECT COUNT(*) FROM `teams_quest` WHERE `user_id`=%s"
         self.request = self.query(sql, user_id)
         self.result = self.request.fetchone()
         print("Db().count(): Counted")
@@ -37,23 +37,23 @@ class Db:
             return True
         return False
 
-    def create_user(self, user_id, name=""):
+    def create_team(self, user_id, name=""):
         exist = self.count(user_id)
         if not exist:
-            sql = "INSERT INTO `leader_users` (`user_id`, `name`) VALUES (%s, %s)"
+            sql = "INSERT INTO `teams_quest` (`user_id`, `name`) VALUES (%s, %s)"
             self.request = self.query(sql, (user_id, name))
-            sql = "INSERT INTO `points_table` (`user_id`, `current_question`) VALUES (%s, %s)"
+            sql = "INSERT INTO `points_table_quest` (`user_id`, `current_question`) VALUES (%s, %s)"
             self.request = self.query(sql, (user_id, 0))
-            print("Db().create_user(): Created")
+            print("Db().create_team(): Created")
         else:
-            sql = "UPDATE `leader_users` SET `name`=%s WHERE `user_id`=%s"
+            sql = "UPDATE `teams_quest` SET `name`=%s WHERE `user_id`=%s"
             self.request = self.query(sql, (name, user_id))
-            print("Db().create_user(): Updated")
+            print("Db().create_team(): Updated")
 
     def update_name(self, user_id, name=""):
         exist = self.count(user_id)
         if exist:
-            sql = "UPDATE `leader_users` SET `name`=%s WHERE `user_id`=%s"
+            sql = "UPDATE `teams_quest` SET `name`=%s WHERE `user_id`=%s"
             self.request = self.query(sql, (name, user_id))
             print("Db().update_name(): Updated")
             return 1
@@ -63,7 +63,7 @@ class Db:
     def get_name(self, user_id):
         exist = self.count(user_id)
         if exist:
-            sql = "SELECT * FROM `leader_users` WHERE `user_id`=%s"
+            sql = "SELECT * FROM `teams_quest` WHERE `user_id`=%s"
             self.request = self.query(sql, user_id)
             self.result = self.request.fetchone()
             print("Db().get_name(): Got")
@@ -71,17 +71,44 @@ class Db:
         else:
             return ""
 
+    def add_contact_face(self, user_id, contact_face):
+        exist = self.count(user_id)
+        if exist:
+            sql = "UPDATE `teams_quest` SET `contact_face`=%s WHERE `user_id`=%s"
+            self.request = self.query(sql, (contact_face, user_id))
+            print("Db().add_contact_face(): Added")
+        else:
+            print("Db().add_contact_face(): User doesn't exist")
+
+    def add_link_vk(self, user_id, link_vk):
+        exist = self.count(user_id)
+        if exist:
+            sql = "UPDATE `teams_quest` SET `link_vk`=%s WHERE `user_id`=%s"
+            self.request = self.query(sql, (link_vk, user_id))
+            print("Db().add_link_vk(): Added")
+        else:
+            print("Db().add_link_vk(): User doesn't exist")
+
+    def add_tel_number(self, user_id, tel_number):
+        exist = self.count(user_id)
+        if exist:
+            sql = "UPDATE `teams_quest` SET `tel_number`=%s WHERE `user_id`=%s"
+            self.request = self.query(sql, (tel_number, user_id))
+            print("Db().add_tel_number(): Added")
+        else:
+            print("Db().add_tel_number(): User doesn't exist")
+
     def add_institute(self, user_id, institute=""):
         exist = self.count(user_id)
         if exist:
-            sql = "UPDATE `leader_users` SET `institute`=%s WHERE `user_id`=%s"
+            sql = "UPDATE `teams_quest` SET `institute`=%s WHERE `user_id`=%s"
             self.request = self.query(sql, (institute, user_id))
             print("Db().add_institute(): Added")
         else:
             print("Db().add_institute(): User doesn't exist")
 
     def get_result(self, user_id):
-        sql = "SELECT `result` FROM `points_table` WHERE `user_id`=%s"
+        sql = "SELECT `result` FROM `points_table_quest` WHERE `user_id`=%s"
         self.request = self.query(sql, user_id)
         self.result = self.request.fetchone()
         print("Db().get_result(): Done")
@@ -89,7 +116,7 @@ class Db:
         return int(self.result['result'])
 
     def get_current_question(self, user_id: int) -> int:
-        sql = "SELECT `current_question` FROM `points_table` WHERE `user_id`=%s"
+        sql = "SELECT `current_question` FROM `points_table_quest` WHERE `user_id`=%s"
         self.request = self.query(sql, user_id)
         self.result = self.request.fetchone()
         print("Db().get_current_question(): Done")
@@ -99,38 +126,18 @@ class Db:
     def set_point(self, user_id: int, question: int):
         result = self.get_result(user_id)
         new_result = result + 1
-        sql = "UPDATE `points_table` SET `%s`='1', `result`=%s WHERE `user_id`=%s"
+        sql = "UPDATE `points_table_quest` SET `%s`='1', `result`=%s WHERE `user_id`=%s"
         self.request = self.query(sql, (question, str(new_result), user_id))
         print("Db().set_point(): Done")
 
     def increment_current_question(self, user_id: int, current_question: int):
         new_question = current_question + 1
-        sql = "UPDATE `points_table` SET `current_question`=%s WHERE `user_id`=%s"
+        sql = "UPDATE `points_table_quest` SET `current_question`=%s WHERE `user_id`=%s"
         self.request = self.query(sql, (str(new_question), user_id))
         print("Db().increment_current_question(): Done")
 
-    def have_hints(self, user_id):
-        sql = "SELECT `count_of_hints` FROM `points_table` WHERE `user_id`=%s"
-        self.request = self.query(sql, user_id)
-        self.result = self.request.fetchone()
-        print("Db().get_count_of_hints(): Done")
-
-        if int(self.result['count_of_hints']) > 0:
-            return True
-        return False
-
-    def set_hint(self, user_id):
-        sql = "UPDATE `points_table` SET `count_of_hints`='1' WHERE `user_id`=%s"
-        self.request = self.query(sql, user_id)
-        print("Db().set_hint(): Done")
-
-    def delete_hint(self, user_id):
-        sql = "UPDATE `points_table` SET `count_of_hints`='0' WHERE `user_id`=%s"
-        self.request = self.query(sql, user_id)
-        print("Db().delete_hint(): Done")
-
     def get_users(self):
-        sql = "SELECT * FROM `leader_users`"
+        sql = "SELECT * FROM `teams_quest`"
         self.request = self.query(sql)
         self.result = self.request.fetchall()
         print("Db().get_users(): Done")
